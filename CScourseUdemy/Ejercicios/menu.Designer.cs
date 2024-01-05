26d1b292be2e726c1d7df8470a27e25e49674217
Practicante Sistemas <practicanteit@accudyn.com>
2024-01-05 11:47:01 -0600

    Menu principal inicio

---
+++ CScourseUdemy/Ejercicios/menu.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace CScourseUdemy.Ejercicios
+{
+    partial class menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            button1 = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            panel1.SuspendLayout();
+            flowLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(flowLayoutPanel1);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(192, 420);
+            panel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(button2);
+            flowLayoutPanel1.Controls.Add(button1);
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.ForeColor = SystemColors.ActiveCaptionText;
+            flowLayoutPanel1.Location = new Point(0, 30);
+            flowLayoutPanel1.Margin = new Padding(0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(192, 390);
+            flowLayoutPanel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(54, 54, 54);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(172, 52);
+            button1.TabIndex = 0;
+            button1.Text = "Ejercicio 1\r\n{Operadores logicos}";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.Dock = DockStyle.Top;
+            label1.Font = new Font("Consolas", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(192, 30);
+            label1.TabIndex = 1;
+            label1.Text = "Ejercicios";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(54, 54, 54);
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(6, 7);
+            button2.Margin = new Padding(0);
+            button2.Name = "button2";
+            button2.Size = new Size(172, 52);
+            button2.TabIndex = 1;
+            button2.Text = "Ejercicio 1\r\n{Operadores logicos}";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // menu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 14F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(27, 27, 27);
+            ClientSize = new Size(800, 420);
+            Controls.Add(panel1);
+            Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ForeColor = SystemColors.ControlLight;
+            Name = "menu";
+            Text = "menu";
+            panel1.ResumeLayout(false);
+            flowLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button button1;
+        private Button button2;
+    }
+}
