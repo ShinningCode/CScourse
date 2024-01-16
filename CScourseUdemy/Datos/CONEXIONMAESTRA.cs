eaf66289b0f52660ffb8fc2c180afe5f813dadd7
unknown <a32997@uach.mx>
2024-01-16 12:20:34 -0600

    Se arreglaron errores

--- CScourseUdemy/Datos/CONEXIONMAESTRA.cs
+++ CScourseUdemy/Datos/CONEXIONMAESTRA.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
 using System.Data.SqlClient;
-using System.Data;
 
 namespace CScourseUdemy.Datos
 {
     public class CONEXIONMAESTRA
-    {                              //----------NOMBRE DE DISPOSITIVO------------NOMBRE BASE DE DATOS
+    {
+        // Connection string with Data Source, Initial Catalog, and Integrated Security settings
         public static string CONEXION = @"Data Source=DESKTOP-D6JQ9NG\SQLEXPRESS; Initial Catalog=BASECURSO; Integrated Security=true";
+
+        // SqlConnection object for database connection
         public static SqlConnection conectar = new SqlConnection(CONEXION);
 
+        // Method to open the database connection
         public static void abrir()
         {
             if (conectar.State == ConnectionState.Closed)
@@ -20,6 +19,8 @@ namespace CScourseUdemy.Datos
                 conectar.Open();
             }
         }
+
+        // Method to close the database connection
         public static void cerrar()
         {
             if (conectar.State == ConnectionState.Open)
@@ -27,6 +28,5 @@ namespace CScourseUdemy.Datos
                 conectar.Close();
             }
         }
-    }
     }
 }
